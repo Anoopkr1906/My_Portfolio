be0afcf544e2ba86330d54539b012068f56d3332
Anoop Kumar Burnwal <anoopkumar19062006@gmail.com>
2025-06-17 19:29:30 +0530

    Mofified ProfileCard

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -7,7 +7,7 @@ function ProfileCard() {
         <div className='flex flex-col items-center  justify-center text-center text-black space-y-4 p-6 animate-bounce hover:animate-none'
             style={{ fontFamily: "Fondamento , cursive" }}>
             <p className='m-2 text-4xl'>ANOOP KUMAR BURNWAL</p>
-            <p className='m-2 text-2xl'>An Undergrad Student at NIT JSR</p>
+            <p className='m-2 text-2xl'>An Undergrad Student at NIT JSR :) </p>
         </div>
         <div class="w-108 h-108 rounded-full bg-cover bg-center border-3 border-amber-800 animate-pulse hover:animate-none" style={{ backgroundImage: `url(${AnoopImg})` }}>
         </div>
